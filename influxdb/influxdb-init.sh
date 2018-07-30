f544716aa637b4d83f59d9103311bbd70c8c9787
Andras Sebo <andras.sebo@gmail.com>
2018-07-30 21:28:36 +0200

    finetune influxdb config to use less mem / cpu

--- influxdb/influxdb-init.sh
+++ influxdb/influxdb-init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 curl -i -XPOST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE collectd"
 curl -i -XPOST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE cadvisor"
+
+# set retention policies
+ curl -i -XPOST http://localhost:8086/query --data-urlencode "q=CREATE RETENTION POLICY "one_week_only" ON "collectd" DURATION 1w REPLICATION 1 DEFAULT"
+ curl -i -XPOST http://localhost:8086/query --data-urlencode "q=CREATE RETENTION POLICY "one_week_only" ON "cadvisor" DURATION 1w REPLICATION 1 DEFAULT"
